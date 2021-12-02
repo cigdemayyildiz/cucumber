c2b2039b2d050c7cba24139bf026d0b7f30fefbe
Cigdem <cigdemrespect@gmail.com>
2021-12-02 14:42:57 -0600

    Amazon Step Definitions

--- src/test/java/step_definitions/AmazonStepDefs.java
+++ src/test/java/step_definitions/AmazonStepDefs.java
@@ -36,5 +36,4 @@ public class AmazonStepDefs {
         amazonSearchResultsPage = new AmazonSearchResultsPage(driver);
         Assert.assertEquals(amazonSearchResultsPage.getResultNumber(), 20000);
     }
-
 }
